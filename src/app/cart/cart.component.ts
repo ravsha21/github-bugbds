dd03aeb1983dd01168c409f9f1cf54c108872c71
ravsha21 <sharmaraveena03@gmail.com>
2020-05-28 23:26:22 +1200

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor(
-    private cartService: CartService
-  ) { }
+  constructor() { }
 
   ngOnInit() {
   }
